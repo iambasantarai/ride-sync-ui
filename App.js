4f02a86fb78d89fa7bd9ce790104e89509ef9a48
iambasantarai <iambasantarai@gmail.com>
2023-12-16 18:34:00 +0545

    Add and load fonts

--- App.js
+++ App.js
@@ -1,11 +1,34 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useCallback } from "react";
+import { Text, View, StyleSheet } from "react-native";
+import { useFonts } from "expo-font";
+import * as SplashScreen from "expo-splash-screen";
+
+SplashScreen.preventAutoHideAsync();
 
 export default function App() {
+  const [fontsLoaded, fontError] = useFonts({
+    "hn-light": require("./assets/fonts/HelveticaNeueLight.otf"),
+    "hn-normal": require("./assets/fonts/HelveticaNeueRoman.otf"),
+    "hn-semibold": require("./assets/fonts/HelveticaNeueMedium.otf"),
+    "hn-bold": require("./assets/fonts/HelveticaNeueBold.otf"),
+    "russo-one": require("./assets/fonts/RussoOneRegular.ttf"),
+  });
+
+  const onLayoutRootView = useCallback(async () => {
+    if (fontsLoaded || fontError) {
+      await SplashScreen.hideAsync();
+    }
+  }, [fontsLoaded, fontError]);
+
+  if (!fontsLoaded && !fontError) {
+    return null;
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={styles.container} onLayout={onLayoutRootView}>
+      <Text style={{ fontFamily: "russo-one", fontSize: 30 }}>
+        Get. Set. Go!
+      </Text>
     </View>
   );
 }
@@ -13,8 +36,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });

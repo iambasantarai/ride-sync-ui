6bda69609f1b730762e385d2c63f9d0f07fa7d7c
iambasantarai <iambasantarai@gmail.com>
2023-12-16 21:27:27 +0545

    Create onboarding screen

--- App.js
+++ App.js
@@ -1,11 +1,39 @@
 import { useCallback } from "react";
-import { Text, View, StyleSheet } from "react-native";
+import {
+  Text,
+  StyleSheet,
+  SafeAreaView,
+  TouchableOpacity,
+  Image,
+  View,
+} from "react-native";
 import { useFonts } from "expo-font";
 import * as SplashScreen from "expo-splash-screen";
 
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+
 SplashScreen.preventAutoHideAsync();
 
-export default function App() {
+const onBoardingImage = require("./assets/friends.png");
+const Stack = createNativeStackNavigator();
+
+const App = () => {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen
+          component={Main}
+          name="Main"
+          options={{ headerShown: false }}
+        />
+        <Stack.Screen component={Home} name="Home" />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+};
+
+const Main = ({ navigation }) => {
   const [fontsLoaded, fontError] = useFonts({
     "hn-light": require("./assets/fonts/HelveticaNeueLight.otf"),
     "hn-normal": require("./assets/fonts/HelveticaNeueRoman.otf"),
@@ -25,19 +53,86 @@ export default function App() {
   }
 
   return (
-    <View style={styles.container} onLayout={onLayoutRootView}>
-      <Text style={{ fontFamily: "russo-one", fontSize: 30 }}>
-        Get. Set. Go!
+    <SafeAreaView onLayout={onLayoutRootView} style={styles.container}>
+      <Text style={styles.title}>
+        Create, join, start and track rides with friends.
       </Text>
-    </View>
+
+      <Image
+        source={onBoardingImage}
+        style={{
+          height: "60%",
+          width: "80%",
+          resizeMode: "contain",
+        }}
+      />
+
+      <TouchableOpacity
+        onPress={() => navigation.navigate("Home")}
+        style={styles.button}
+      >
+        <Text style={styles.buttonText}>Get Started</Text>
+      </TouchableOpacity>
+
+      <Text style={styles.helper}>
+        Already have an account? <Text style={styles.link}>Login</Text>
+      </Text>
+    </SafeAreaView>
   );
-}
+};
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#fff",
     alignItems: "center",
-    justifyContent: "center",
+    backgroundColor: "#11181E",
+  },
+
+  title: {
+    fontSize: 30,
+    color: "#FFFFFF",
+    fontFamily: "russo-one",
+    textAlign: "left",
+    marginTop: 24,
+    marginHorizontal: 20,
+  },
+
+  button: {
+    backgroundColor: "#469C67",
+    padding: 15,
+    width: "90%",
+    borderRadius: 16,
+    alignItems: "center",
+  },
+
+  buttonText: {
+    color: "#FFFFFF",
+    fontFamily: "rosso-one",
+    fontSize: 24,
+    fontWeight: "bold",
+  },
+
+  helper: {
+    margin: 20,
+    color: "#FFFFFF",
+    fontFamily: "hn-normal",
+    fontSize: 14,
+  },
+
+  link: {
+    color: "#469C67",
+    fontFamily: "hn-normal",
+    fontSize: 14,
+    fontWeight: "700",
   },
 });
+
+const Home = () => {
+  return (
+    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+      <Text>Home Screen </Text>
+    </View>
+  );
+};
+
+export default App;

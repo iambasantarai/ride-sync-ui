0500e7223ce17392ddefc6253eebbc243c870688
iambasantarai <iambasantarai@gmail.com>
2023-12-22 21:29:21 +0545

    Get logged in user profile

--- screens/ProfileScreen.js
+++ screens/ProfileScreen.js
@@ -1,11 +1,33 @@
-import React from "react";
-import { View, Text, StyleSheet } from "react-native";
+import React, { useEffect, useState } from "react";
+import { View, Text, StyleSheet, Image } from "react-native";
 import { COLORS } from "../constants/colors";
+import { apiService } from "../services/apiService";
 
 const ProfileScreen = () => {
+  const [user, setUser] = useState({});
+  useEffect(() => {
+    const getProfile = async () => {
+      const response = await apiService.get("/users/profile");
+      setUser(response.data.user);
+    };
+    getProfile();
+  }, []);
+
   return (
     <View style={styles.container}>
-      <Text style={styles.text}>Profile Screen</Text>
+      <View style={{ padding: 20 }}>
+        <Image
+          source={require("../assets/whoknows.png")}
+          style={{
+            height: 80,
+            width: 80,
+            borderRadius: 40,
+            marginBottom: 10,
+          }}
+        />
+      </View>
+      <Text style={styles.username}>{user.username}</Text>
+      <Text style={styles.email}>{user.email}</Text>
     </View>
   );
 };
@@ -17,9 +39,17 @@ const styles = StyleSheet.create({
     alignItems: "center",
     backgroundColor: COLORS.darkGray,
   },
-  text: {
+  username: {
     fontFamily: "Roboto",
+    fontWeight: "bold",
+    fontSize: 24,
     color: COLORS.white,
+  },
+
+  email: {
+    fontFamily: "Roboto",
+    fontSize: 16,
+    color: COLORS.lightGray,
   },
 });
 

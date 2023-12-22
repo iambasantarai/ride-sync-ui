1f380e96a1759871b71837715602e9ef7a9d95ec
iambasantarai <iambasantarai@gmail.com>
2023-12-22 21:33:51 +0545

    Add explore screen

--- navigation/TabNavigator.js
+++ navigation/TabNavigator.js
@@ -4,7 +4,10 @@ import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import HomeScreen from "../screens/HomeScreen";
 import RoomsScreen from "../screens/RoomsScreen";
 import FriendsScreen from "../screens/FriendsScreen";
+import ExploreScreen from "../screens/ExploreScreen";
 import { FontAwesome5 } from "@expo/vector-icons";
+import { MaterialIcons } from "@expo/vector-icons";
+
 import { COLORS } from "../constants/colors";
 
 const Tab = createBottomTabNavigator();
@@ -26,6 +29,15 @@ const TabNavigator = () => {
         options={{
           tabBarIcon: ({ color, size }) => (
             <FontAwesome5 name="home" color={color} size={size} />
+          ),
+        }}
+      />
+      <Tab.Screen
+        name="Explore"
+        component={ExploreScreen}
+        options={{
+          tabBarIcon: ({ color, size }) => (
+            <MaterialIcons name="explore" color={color} size={size} />
           ),
         }}
       />

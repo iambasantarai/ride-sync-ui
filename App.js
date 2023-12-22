16699065d69c1821acdb75595622dc2c33942db5
iambasantarai <iambasantarai@gmail.com>
2023-12-22 22:04:02 +0545

    Fix statusbar

--- App.js
+++ App.js
@@ -1,11 +1,13 @@
 import "react-native-gesture-handler";
 import { AuthProvider } from "./context/AuthContext";
 import { AppNav } from "./navigation/AppNav";
+import { StatusBar } from "expo-status-bar";
 
 const App = () => {
   return (
     <AuthProvider>
       <AppNav />
+      <StatusBar style="light" />
     </AuthProvider>
   );
 };

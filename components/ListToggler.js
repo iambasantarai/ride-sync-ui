4b0547fc9ed367a5a3855ba11bd9ff58d23b8f23
iambasantarai <iambasantarai@gmail.com>
2023-12-26 21:20:19 +0545

    Add list toggler for friends & requests

---
+++ components/ListToggler.js
@@ -0,0 +1,79 @@
+import React, { useState } from "react";
+import { View, Text, TouchableOpacity } from "react-native";
+import { COLORS } from "../constants/colors";
+
+export const ListToggler = ({
+  selectionMode,
+  firstOption,
+  secondOption,
+  onSelectList,
+}) => {
+  const [getSelectionMode, setSelectionMode] = useState(selectionMode);
+
+  const updateSwitchList = (value) => {
+    setSelectionMode(value);
+    onSelectList(value);
+  };
+
+  return (
+    <View
+      style={{
+        height: 44,
+        width: "100%",
+        backgroundColor: COLORS.charcol,
+        borderRadius: 16,
+        borderColor: COLORS.lightCharcol,
+        flexDirection: "row",
+        justifyContent: "center",
+      }}
+    >
+      <TouchableOpacity
+        activeOpacity={1}
+        onPress={() => updateSwitchList(1)}
+        style={{
+          flex: 1,
+          backgroundColor:
+            getSelectionMode === 1 ? COLORS.green : COLORS.charcol,
+          borderRadius: 16,
+          justifyContent: "center",
+          alignItems: "center",
+        }}
+      >
+        <Text
+          style={{
+            color: getSelectionMode === 1 ? COLORS.white : COLORS.lightGray,
+            fontSize: 16,
+            fontWeight: "700",
+            fontFamily: "Roboto",
+          }}
+        >
+          {firstOption}
+        </Text>
+      </TouchableOpacity>
+
+      <TouchableOpacity
+        activeOpacity={1}
+        onPress={() => updateSwitchList(2)}
+        style={{
+          flex: 1,
+          backgroundColor:
+            getSelectionMode === 2 ? COLORS.green : COLORS.charcol,
+          borderRadius: 16,
+          justifyContent: "center",
+          alignItems: "center",
+        }}
+      >
+        <Text
+          style={{
+            color: getSelectionMode === 2 ? COLORS.white : COLORS.lightGray,
+            fontSize: 16,
+            fontWeight: "700",
+            fontFamily: "Roboto",
+          }}
+        >
+          {secondOption}
+        </Text>
+      </TouchableOpacity>
+    </View>
+  );
+};
--- screens/FriendsScreen.js
+++ screens/FriendsScreen.js
@@ -1,25 +1,150 @@
-import React from "react";
-import { View, Text, StyleSheet } from "react-native";
+import React, { useEffect, useState } from "react";
+import {
+  View,
+  Text,
+  ScrollView,
+  StyleSheet,
+  TouchableOpacity,
+} from "react-native";
 import { COLORS } from "../constants/colors";
+import { SafeAreaProvider } from "react-native-safe-area-context";
+import { ProfileHeader } from "../components/ProfileHeader";
+import HeaderText from "../components/HeaderText";
+import { Feather, Ionicons } from "@expo/vector-icons";
 
-const FriendsScreen = () => {
+import { apiService } from "../services/apiService";
+import { ListToggler } from "../components/ListToggler";
+
+const FriendsScreen = ({ navigation }) => {
+  const [friends, setFriends] = useState([]);
+  const [listTab, setListTab] = useState(1);
+  useEffect(() => {
+    const getFriends = async () => {
+      try {
+        const response = await apiService.get("/friends");
+        setFriends(response.data.friends);
+      } catch (error) {
+        console.log("ERROR: ", error);
+      }
+    };
+    getFriends();
+  }, []);
+
+  const onSelectList = (value) => {
+    setListTab(value);
+  };
+
   return (
-    <View style={styles.container}>
-      <Text style={styles.text}>Friends Screen</Text>
-    </View>
+    <SafeAreaProvider style={styles.container}>
+      <ScrollView style={{ padding: 20 }}>
+        <ProfileHeader navigation={navigation} />
+
+        <View>
+          <ListToggler
+            selectionMode={1}
+            firstOption={"Friends (10)"}
+            secondOption={"Requests (3)"}
+            onSelectList={onSelectList}
+          />
+        </View>
+
+        <View>
+          {listTab === 1 && (
+            <View style={styles.requestCard}>
+              <View style={styles.header}>
+                <View style={styles.headerContnet}>
+                  <Text style={styles.title}>Basanta Rai</Text>
+                  <Text style={styles.subtitle}>basanta@yopmail.com</Text>
+                </View>
+              </View>
+
+              <View style={styles.actionButtons}>
+                <TouchableOpacity style={styles.actionButton}>
+                  <Ionicons
+                    name="ios-person-add"
+                    size={24}
+                    color={COLORS.lightCharcol}
+                  />
+                </TouchableOpacity>
+              </View>
+            </View>
+          )}
+          {listTab === 2 && (
+            <View style={styles.requestCard}>
+              <View style={styles.header}>
+                <View style={styles.headerContnet}>
+                  <Text style={styles.title}>Basanta Rai</Text>
+                  <Text style={styles.subtitle}>basanta@yopmail.com</Text>
+                </View>
+              </View>
+
+              <View style={styles.actionButtons}>
+                <TouchableOpacity style={styles.actionButton}>
+                  <Feather name="x" size={20} color={COLORS.lightCharcol} />
+                </TouchableOpacity>
+
+                <TouchableOpacity style={styles.actionButton}>
+                  <Feather name="check" size={20} color={COLORS.green} />
+                </TouchableOpacity>
+              </View>
+            </View>
+          )}
+        </View>
+      </ScrollView>
+    </SafeAreaProvider>
   );
 };
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
+    paddingTop: 24,
     backgroundColor: COLORS.darkGray,
   },
   text: {
     fontFamily: "Roboto",
     color: COLORS.white,
+  },
+  requestCard: {
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "space-between",
+    marginVertical: 20,
+    padding: 10,
+    borderRadius: 16,
+    backgroundColor: COLORS.charcol,
+  },
+
+  header: {
+    display: "flex",
+    flexDirection: "row",
+  },
+
+  headerContnet: {
+    marginHorizontal: 20,
+  },
+
+  title: {
+    fontSize: 16,
+    color: COLORS.white,
+    fontWeight: "800",
+    fontFamily: "Roboto",
+  },
+
+  subtitle: {
+    fontSize: 12,
+    color: COLORS.lightCharcol,
+    fontWeight: "600",
+    fontFamily: "Roboto",
+  },
+
+  actionButtons: {
+    flexDirection: "row",
+    alignItems: "center",
+  },
+
+  actionButton: {
+    marginHorizontal: 8,
   },
 });
 

598b7d048ad4ffb63726a92d2c76e8ec29730510
iambasantarai <iambasantarai@gmail.com>
2024-01-01 17:10:01 +0545

    Create a dashed outline around the button

--- screens/RoomsScreen.js
+++ screens/RoomsScreen.js
@@ -1,25 +1,62 @@
 import React from "react";
-import { View, Text, StyleSheet } from "react-native";
+import {
+  View,
+  Text,
+  ScrollView,
+  StyleSheet,
+  TouchableOpacity,
+  ToastAndroid,
+} from "react-native";
 import { COLORS } from "../constants/colors";
+import { Ionicons } from "@expo/vector-icons";
+import { SafeAreaProvider } from "react-native-safe-area-context";
+import { ProfileHeader } from "../components/ProfileHeader";
 
-const RoomsScreen = () => {
+const RoomsScreen = ({ navigation }) => {
   return (
-    <View style={styles.container}>
-      <Text style={styles.text}>Rooms Screen</Text>
-    </View>
+    <SafeAreaProvider style={styles.container}>
+      <ScrollView style={{ padding: 20 }}>
+        <ProfileHeader navigation={navigation} />
+        <View style={styles.dashedContainer}>
+          <TouchableOpacity style={styles.createButton}>
+            <Text style={styles.buttonText}>Create new room</Text>
+            <Ionicons name="add-circle" size={48} color={COLORS.lightCharcol} />
+          </TouchableOpacity>
+        </View>
+      </ScrollView>
+    </SafeAreaProvider>
   );
 };
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
+    paddingTop: 24,
     backgroundColor: COLORS.darkGray,
   },
   text: {
     fontFamily: "Roboto",
     color: COLORS.white,
+  },
+  dashedContainer: {
+    borderWidth: 2,
+    borderRadius: 16,
+    paddingVertical: 8,
+    paddingHorizontal: 16,
+    backgroundColor: COLORS.charcol,
+    borderColor: COLORS.lightCharcol,
+    borderStyle: "dashed",
+  },
+  createButton: {
+    padding: 8,
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+  },
+  buttonText: {
+    fontFamily: "Roboto",
+    color: COLORS.lightCharcol,
+    fontSize: 24,
   },
 });
 

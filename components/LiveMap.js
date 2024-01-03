2cf832d9baed649a38be7d0c583f0609a816983f
iambasantarai <iambasantarai@gmail.com>
2024-01-03 21:00:33 +0545

    Use safe area view to render map

--- components/LiveMap.js
+++ components/LiveMap.js
@@ -4,31 +4,34 @@ import { StyleSheet, View } from "react-native";
 import mapStyle from "../assets/mapStyle.json";
 import { COLORS } from "../constants/colors";
 import { UserLocationContext } from "../context/UserLocationContext";
+import { SafeAreaProvider } from "react-native-safe-area-context";
 
 const LiveMap = () => {
   const { location, setLocation } = useContext(UserLocationContext);
   return (
     location?.latitude && (
-      <View style={styles.container}>
-        <MapView
-          customMapStyle={mapStyle}
-          style={styles.map}
-          provider={PROVIDER_GOOGLE}
-          region={{
-            latitude: location?.latitude,
-            longitude: location?.longitude,
-            latitudeDelta: 0.04,
-            longitudeDelta: 0.06,
-          }}
-        >
-          <Marker
-            coordinate={{
+      <SafeAreaProvider style={styles.container}>
+        <View>
+          <MapView
+            customMapStyle={mapStyle}
+            style={styles.map}
+            provider={PROVIDER_GOOGLE}
+            region={{
               latitude: location?.latitude,
               longitude: location?.longitude,
+              latitudeDelta: 0.04,
+              longitudeDelta: 0.06,
             }}
-          />
-        </MapView>
-      </View>
+          >
+            <Marker
+              coordinate={{
+                latitude: location?.latitude,
+                longitude: location?.longitude,
+              }}
+            />
+          </MapView>
+        </View>
+      </SafeAreaProvider>
     )
   );
 };
